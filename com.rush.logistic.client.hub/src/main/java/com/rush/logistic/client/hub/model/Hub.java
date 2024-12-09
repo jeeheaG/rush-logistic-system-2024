c1a98ba2332e4d4ba85f77ce83de1c30b5420899
Hany-Kim <hanykim1210@gmail.com>
2024-12-09 17:30:05 +0900

    :hammer: chore: Delete unused code

--- com.rush.logistic.client.hub/src/main/java/com/rush/logistic/client/hub/model/Hub.java
+++ com.rush.logistic.client.hub/src/main/java/com/rush/logistic/client/hub/model/Hub.java
@@ -6,7 +6,6 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import java.time.LocalDateTime;
 import java.util.UUID;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
--- com.rush.logistic.client.hub/src/main/java/com/rush/logistic/client/hub/service/HubService.java
+++ com.rush.logistic.client.hub/src/main/java/com/rush/logistic/client/hub/service/HubService.java
@@ -6,7 +6,6 @@ import com.rush.logistic.client.hub.dto.HubInfoRequestDto;
 import com.rush.logistic.client.hub.message.HubMessage;
 import com.rush.logistic.client.hub.model.Hub;
 import com.rush.logistic.client.hub.repository.HubRepository;
-import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
@@ -29,9 +28,7 @@ public class HubService {
         // 허브 생성 반환
         HubIdResponseDto responseDto = HubIdResponseDto.from(hub.getHubId());
 
-        BaseResponseDto<HubIdResponseDto> response = BaseResponseDto
+        return BaseResponseDto
                 .<HubIdResponseDto>from(HttpStatus.CREATED.value(), HttpStatus.CREATED, HubMessage.HUB_CREATED_SUCCESS.getMessage(), responseDto);
-
-        return response;
     }
 }
