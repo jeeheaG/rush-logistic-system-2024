668e59b638032d396c7585750182cc19398a5690
Hxxseung <soaorsith2@naver.com>
2024-12-12 11:39:50 +0900

    :recycle: move : move file

--- com.rush.logistic.client.company-product/src/main/java/com/rush/logistic/client/company_product/global/config/JpaConfig.java
+++ com.rush.logistic.client.company-product/src/main/java/com/rush/logistic/client/company_product/global/config/JpaConfig.java
@@ -1,4 +1,4 @@
-package com.rush.logistic.client.company_product.domain.config;
+package com.rush.logistic.client.company_product.global.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
--- com.rush.logistic.client.company-product/src/main/java/com/rush/logistic/client/company_product/global/entity/BaseEntity.java
+++ com.rush.logistic.client.company-product/src/main/java/com/rush/logistic/client/company_product/global/entity/BaseEntity.java
@@ -1,4 +1,4 @@
-package com.rush.logistic.client.company_product.domain.entity;
+package com.rush.logistic.client.company_product.global.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
