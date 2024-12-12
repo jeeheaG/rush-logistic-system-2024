586aa2db865f0470367b4e79ddbccec39c3b0a76
Hany-Kim <hanykim1210@gmail.com>
2024-12-12 09:50:59 +0900

    :sparkles: feat: HUB-to-HUB route creation API linked with Naver MAP API

---
+++ com.rush.logistic.client.hub/src/main/java/com/rush/logistic/client/hub/dto/TimeTakenAndDistDto.java
@@ -0,0 +1,15 @@
+package com.rush.logistic.client.hub.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class TimeTakenAndDistDto {
+    private String timeTaken;
+    private String distance;
+}
