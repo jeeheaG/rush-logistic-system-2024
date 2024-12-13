400aa0b864b1934ea5c3c2e73545cd7c30748f34
jeeheaG <jeehea99@naver.com>
2024-12-13 19:17:17 +0900

    :sparkles: add : make api call util classes

---
+++ com.rush.logistic.client.order-delivery/src/main/java/com/rush/logistic/client/order_delivery/global/util/navermap/NaverMapUtil.java
@@ -0,0 +1,10 @@
+package com.rush.logistic.client.order_delivery.global.util.navermap;
+
+import org.springframework.stereotype.Component;
+
+public class NaverMapUtil {
+    public static NaverMapRes getDistanceAndTimeByAddress(String startAddress, String endAddress) {
+        // api call 로직 구현
+        return NaverMapRes.toDto();
+    }
+}
