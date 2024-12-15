8d83832e25c4a9de76878603ca4f77bf12be004d
jeeheaG <jeehea99@naver.com>
2024-12-15 14:54:45 +0900

    :sparkles: add : status code of auth

--- com.rush.logistic.client.order-delivery/src/main/java/com/rush/logistic/client/order_delivery/global/response/BasicCode.java
+++ com.rush.logistic.client.order-delivery/src/main/java/com/rush/logistic/client/order_delivery/global/response/BasicCode.java
@@ -11,8 +11,12 @@ public enum BasicCode implements StatusCode{
     TEST_OK(HttpStatus.OK, "성공 (test)"),
 
     ILLEGAL_ARGUMENT_ERROR(HttpStatus.BAD_REQUEST, "유효하지 않은 파라미터"),
+
     CANNOT_RESOLVE_USER_ROLE(HttpStatus.UNAUTHORIZED, "사용자 권한을 알 수 없음"),
-    CANNOT_RESOLVE_USER_ID(HttpStatus.UNAUTHORIZED, "사용자 식별자를 알 수 없음");
+    CANNOT_RESOLVE_USER_ID(HttpStatus.UNAUTHORIZED, "사용자 식별자를 알 수 없음"),
+    TOKEN_USER_ROLE_NOT_VALID(HttpStatus.UNAUTHORIZED, "사용자의 해당 권한이 더 이상 유효하지 않음"),
+    USER_NOT_ALLOWED(HttpStatus.FORBIDDEN, "허가되지 않은 사용자의 접근"),
+    CANNOT_CHECK_USER_ROLE_IN_CHARGE(HttpStatus.FORBIDDEN, "사용자가 해당 리소스 담당인지 알 수 없음");
 
     private final HttpStatus httpStatus;
     private final String message;
