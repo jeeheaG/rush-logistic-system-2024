d29fe06ee2bc2b1cb0880532057010aa6de9ea6e
wkdehf217 <wkdehfdl17@naver.com>
2024-12-17 12:08:40 +0900

    :sparkles: feat : user, slack Swagger 적용

---
+++ com.rush.logistic.client.slack/src/main/java/com/rush/logistic/client/slack/domain/config/SwaggerConfig.java
@@ -0,0 +1,34 @@
+package com.rush.logistic.client.slack.domain.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customizeOpenAPI() {
+
+        Info info = new Info()
+                .title("RushProject")
+                .description("AI 기반 배송 소요시간 계산 물류 시스템 (MSA)");
+
+
+        return new OpenAPI()
+                .components(new Components().addSecuritySchemes("Bearer",
+                        new io.swagger.v3.oas.models.security.SecurityScheme()
+                                .type(io.swagger.v3.oas.models.security.SecurityScheme.Type.HTTP)
+                                .in(SecurityScheme.In.HEADER)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")))
+                .addSecurityItem(new io.swagger.v3.oas.models.security.SecurityRequirement()
+                        .addList("Bearer"))
+                .addServersItem(new Server().url("/"))
+                .info(info);
+    }
+}
