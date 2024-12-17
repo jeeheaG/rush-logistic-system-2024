c8173d54c57f21e159eca7e2e914396e20e4dc10
jeeheaG <jeehea99@naver.com>
2024-12-17 15:35:31 +0900

    :sparkles: feat : make delivery search api

--- com.rush.logistic.client.order-delivery/src/main/java/com/rush/logistic/client/order_delivery/domain/delivery/controller/DeliveryController.java
+++ com.rush.logistic.client.order-delivery/src/main/java/com/rush/logistic/client/order_delivery/domain/delivery/controller/DeliveryController.java
@@ -1,18 +1,28 @@
 package com.rush.logistic.client.order_delivery.domain.delivery.controller;
 
+import com.querydsl.core.types.Predicate;
 import com.rush.logistic.client.order_delivery.domain.delivery.controller.dto.response.DeliveryIdRes;
 import com.rush.logistic.client.order_delivery.domain.delivery.service.DeliveryService;
 import com.rush.logistic.client.order_delivery.domain.delivery.controller.dto.request.DeliveryAllReq;
 import com.rush.logistic.client.order_delivery.domain.delivery.controller.dto.response.DeliveryAllRes;
 import com.rush.logistic.client.order_delivery.domain.delivery.exception.DeliveryCode;
+import com.rush.logistic.client.order_delivery.domain.delivery_route.controller.dto.response.DeliveryRouteAllRes;
+import com.rush.logistic.client.order_delivery.domain.delivery_route.domain.DeliveryRoute;
+import com.rush.logistic.client.order_delivery.domain.delivery_route.exception.DeliveryRouteCode;
 import com.rush.logistic.client.order_delivery.domain.order.controller.client.dto.response.GetUserInfoRes;
 import com.rush.logistic.client.order_delivery.global.auth.UserInfo;
 import com.rush.logistic.client.order_delivery.global.auth.UserInfoHeader;
 import com.rush.logistic.client.order_delivery.global.auth.UserRole;
-import com.rush.logistic.client.order_delivery.global.auth.checker.OrderUserRoleChecker;
+import com.rush.logistic.client.order_delivery.global.auth.checker.DeliveryUserRoleChecker;
+import com.rush.logistic.client.order_delivery.global.common.SearchUtil;
 import com.rush.logistic.client.order_delivery.global.response.BaseResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.querydsl.binding.QuerydslPredicate;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.data.web.PagedModel;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -25,7 +35,7 @@ import java.util.UUID;
 @RequestMapping("/api/deliveries")
 public class DeliveryController {
     private final DeliveryService deliveryService;
-    private final OrderUserRoleChecker orderUserRoleChecker;
+    private final DeliveryUserRoleChecker deliveryUserRoleChecker;
 
 //    // TODO : 테스트용 임시 api. 원래 delivery create 는  order 에서 내부 요청만 함
 //    @PostMapping
@@ -40,16 +50,28 @@ public class DeliveryController {
     @GetMapping("/{deliveryId}")
     public ResponseEntity<Object> getDeliveryById(@UserInfoHeader UserInfo userInfo, @PathVariable UUID deliveryId) {
         log.info("DeliveryController getDeliveryById");
-        GetUserInfoRes getUserInfoRes = orderUserRoleChecker.getUserAndCheckAllRole(userInfo);
+        GetUserInfoRes getUserInfoRes = deliveryUserRoleChecker.getUserAndCheckAllRole(userInfo);
 
         DeliveryAllRes responseDto = deliveryService.getDeliveryDetail(deliveryId, getUserInfoRes);
         return ResponseEntity.ok().body(BaseResponse.toResponse(DeliveryCode.GET_DELIVERY_OK, responseDto));
     }
 
+    @GetMapping
+    public ResponseEntity<Object> searchDelivery(@UserInfoHeader UserInfo userInfo,
+                                                      @QuerydslPredicate(root = DeliveryRoute.class) Predicate predicate,
+                                                      @PageableDefault(sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable) {
+        log.info("DeliveryController searchDelivery");
+        GetUserInfoRes getUserInfoRes = deliveryUserRoleChecker.getUserAndCheckAllRole(userInfo);
+        Pageable pageRequest = SearchUtil.checkAndGetPageRequest(pageable);
+
+        PagedModel<DeliveryAllRes> responseDtos = deliveryService.getDeliverySearch(getUserInfoRes, predicate, pageRequest);
+        return ResponseEntity.ok().body(BaseResponse.toResponse(DeliveryCode.SEARCH_DELIVERY_OK, responseDtos));
+    }
+
     @PatchMapping("/{deliveryId}")
     public ResponseEntity<Object> updateDelivery(@UserInfoHeader UserInfo userInfo, @PathVariable UUID deliveryId, @RequestBody DeliveryAllReq requestDto) {
         log.info("DeliveryController updateDelivery");
-        GetUserInfoRes getUserInfoRes = orderUserRoleChecker.getUserAndCheckRole(Arrays.asList(UserRole.MASTER, UserRole.HUB, UserRole.DELIVERY), userInfo);
+        GetUserInfoRes getUserInfoRes = deliveryUserRoleChecker.getUserAndCheckRole(Arrays.asList(UserRole.MASTER, UserRole.HUB, UserRole.DELIVERY), userInfo);
 
         DeliveryAllRes responseDto = deliveryService.updateDelivery(deliveryId, requestDto, getUserInfoRes);
         return ResponseEntity.ok().body(BaseResponse.toResponse(DeliveryCode.UPDATE_DELIVERY_OK, responseDto));
@@ -58,7 +80,7 @@ public class DeliveryController {
     @DeleteMapping("/{deliveryId}")
     public ResponseEntity<Object> deleteDelivery(@UserInfoHeader UserInfo userInfo, @PathVariable UUID deliveryId) {
         log.info("DeliveryController deleteDelivery");
-        GetUserInfoRes getUserInfoRes = orderUserRoleChecker.getUserAndCheckRole(Arrays.asList(UserRole.MASTER, UserRole.HUB), userInfo);
+        GetUserInfoRes getUserInfoRes = deliveryUserRoleChecker.getUserAndCheckRole(Arrays.asList(UserRole.MASTER, UserRole.HUB), userInfo);
 
         UUID deletedId = deliveryService.deleteDelivery(deliveryId, userInfo.getUserId(), getUserInfoRes);
         return ResponseEntity.ok().body(BaseResponse.toResponse(DeliveryCode.DELETE_DELIVERY_OK, DeliveryIdRes.toDto(deletedId)));
---
+++ com.rush.logistic.client.order-delivery/src/main/java/com/rush/logistic/client/order_delivery/domain/delivery/repository/DeliveryRepositoryCustom.java
@@ -0,0 +1,4 @@
+package com.rush.logistic.client.order_delivery.domain.delivery.repository;
+
+public interface DeliveryRepositoryCustom {
+}
